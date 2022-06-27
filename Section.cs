a9e12e63bf11f93f40710c4c9c0c7e3a84e55694
Joseph Ereifej <77764253+jereifej@users.noreply.github.com>
2022-06-27 17:00:56 -0400

    TrackModel_v0.1 Directory

---
+++ Section.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackModel_v0._1
+{
+    internal class Section
+    {
+        public Section()
+        {
+            mBlocks = new List<Block>();
+        }
+        public Section(string newName)
+        {
+            mnameSection = newName;
+            mBlocks = new List<Block>();
+        }
+
+        public int getmnumBlocks()
+        {
+            return mnumBlocks;
+        }
+        public string getmnameSection()
+        {
+            return mnameSection;
+        }
+        public string[] getmblockInfo(int blockIdx)
+        {
+            return mBlocks[blockIdx].getmblockInfo();
+        }
+        public List<string> getBlockNum()
+        {
+            List<string> blockNum = new List<string>();
+
+            foreach(Block block in mBlocks)
+            {
+                blockNum.Add("" + block.getmblockNum());
+            }    
+            return blockNum;
+        }
+        public void setmnameSection(string newName)
+        {
+            mnameSection = newName;
+        }
+        public void addBlock(string[] blockInfo)
+        {
+            Block newBlock = new Block(blockInfo);
+            mBlocks.Add(newBlock);
+            mnumBlocks++;
+        }
+
+        int mnumBlocks;
+        string mnameSection;
+        List<Block> mBlocks;
+    }
+}
---
+++ TrackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackModel_v0._1
+{
+    internal class TrackModel
+    {
+        public int main()
+        {
+            MainWindow UI = new MainWindow();
+            UI.Show();
+            return 0;
+        }
+    }
+}
