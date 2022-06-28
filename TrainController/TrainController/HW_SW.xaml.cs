9dbebeaad4121e4e388a410ada55e18c452c5a28
Cheezy16 <techeezum@gmail.com>
2022-06-28 18:16:50 -0400

    Update HW_SW window to enable main window functions only after choosing hardware/software

--- TrainController/TrainController/HW_SW.xaml.cs
+++ TrainController/TrainController/HW_SW.xaml.cs
@@ -28,18 +28,56 @@ namespace TrainController
         {
             if (sender == SoftwareController)
             {
+                // Set controller type to software, and show on main window:
                 ((MainWindow)Application.Current.MainWindow).mControlType = false;
                 ((MainWindow)Application.Current.MainWindow).SelectType.Text = "Software Controller";
-                ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0xDF, 0x20)); ;
+                ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0xDF, 0x20));
+
+                // Enable all manual mode buttons on main window:
+                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).AutoMode.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).ServiceBrake.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).EmergencyBrake.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).SetSpeedBox.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).EngineerPanel.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TestPanel.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TempIncrease.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TempDecrease.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).Announcements.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LeftDoors.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).RightDoors.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LightsInterior.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LightsExterior.IsEnabled = true;
+
+                // Disable both controller type buttons and exit to main window:
                 SoftwareController.IsEnabled = false;
                 HardwareController.IsEnabled = false;
                 this.Close();
             }
             else
             {
+                // Set controller type to hardware, and show on main window:
                 ((MainWindow)Application.Current.MainWindow).mControlType = true;
                 ((MainWindow)Application.Current.MainWindow).SelectType.Text = "Hardware Controller";
-                ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0x5F, 0xA0)); ;
+                ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0x5F, 0xA0));
+
+                // Enable all manual mode buttons on main window:
+                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).AutoMode.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).ServiceBrake.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).EmergencyBrake.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).SetSpeedBox.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).EngineerPanel.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TestPanel.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TempIncrease.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).TempDecrease.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).Announcements.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LeftDoors.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).RightDoors.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LightsInterior.IsEnabled = true;
+                ((MainWindow)Application.Current.MainWindow).LightsExterior.IsEnabled = true;
+
+                // Disable both controller type buttons and exit to main window:
                 SoftwareController.IsEnabled = false;
                 HardwareController.IsEnabled = false;
                 this.Close();
