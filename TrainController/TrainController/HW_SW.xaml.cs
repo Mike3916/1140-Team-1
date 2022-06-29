0a66080ed0b0a369ff182513ab16538bfaaafe75
Benjamin Nguyen <ben30@pitt.edu>
2022-06-28 20:04:45 -0400

    temperature control

--- TrainController/TrainController/HW_SW.xaml.cs
+++ TrainController/TrainController/HW_SW.xaml.cs
@@ -34,7 +34,8 @@ namespace TrainController
                 ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0xDF, 0x20));
 
                 // Enable all manual mode buttons on main window:
-                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = true;
+                // Controller enters manual mode by default
+                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = false;
                 ((MainWindow)Application.Current.MainWindow).AutoMode.IsEnabled = true;
                 ((MainWindow)Application.Current.MainWindow).ServiceBrake.IsEnabled = true;
                 ((MainWindow)Application.Current.MainWindow).EmergencyBrake.IsEnabled = true;
@@ -62,7 +63,8 @@ namespace TrainController
                 ((MainWindow)Application.Current.MainWindow).SelectType.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x8F, 0x5F, 0xA0));
 
                 // Enable all manual mode buttons on main window:
-                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = true;
+                // Controller enters manual mode by default
+                ((MainWindow)Application.Current.MainWindow).ManualMode.IsEnabled = false;
                 ((MainWindow)Application.Current.MainWindow).AutoMode.IsEnabled = true;
                 ((MainWindow)Application.Current.MainWindow).ServiceBrake.IsEnabled = true;
                 ((MainWindow)Application.Current.MainWindow).EmergencyBrake.IsEnabled = true;
