0029fe25d81d1be0803a412fb37450a471ded566
jereifej <JHE7@pitt.edu>
2022-07-29 16:33:09 -0400

    Track Model Test Case

--- Gog/gogTests/UnitTest1.cs
+++ Gog/gogTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using GogNS;
+using TrackModel;
 
 namespace gogTests
 {
@@ -8,6 +9,21 @@ namespace gogTests
         [TestMethod]
         public void TestMethod1()
         {
+        }
+        [TestMethod]
+        public void TrackModel_AddTrain()
+        {
+            int blockIdx = 0;
+            int lineIdx = 0;
+            int authority = 0;
+
+            TrackModel.MainWindow track = new TrackModel.MainWindow();
+            track.AddTrain(blockIdx, lineIdx, authority);
+
+            Assert.AreEqual(track.mtrainList.Count, 1);
+            Assert.AreEqual(track.mtrainList[0].blockIdx, 0);
+            Assert.AreEqual(track.mtrainList[0].lineIdx, 0);
+            Assert.AreEqual(track.mtrainList[0].commAuthority, 0);
         }
     }
 }
