f49426b50b933f3dfaeb5937913ac958ecc533a6
Michael Nowak <MJN56@pitt.edu>
2022-07-29 16:17:48 -0400

    Added a test.

--- Gog/gogTests/UnitTest1.cs
+++ Gog/gogTests/UnitTest1.cs
@@ -9,5 +9,40 @@ namespace gogTests
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+        public void TrackReceiveOccupancies()
+        {
+            Track_Controller_1._02.Controller RedlinePLC = new Track_Controller_1._02.Controller(851, false, "127.0.0.1");
+
+            int[] occupancies = new int[77];
+            int[] returns = new int[77];
+
+            RedlinePLC.SendOccupancies(occupancies);
+            returns = RedlinePLC.ReceiveOccupancies(77);
+
+            for(int i = 0; i < returns.Length; i++)
+            {
+                Assert(occupancies[i] == returns[i]);
+            }
+
+            for (int i = 0; i < returns.Length; i++)
+            {
+                occupancies[i] = 1;
+            }
+
+            RedlinePLC.SendOccupancies(occupancies);
+            returns = RedlinePLC.ReceiveOccupancies(77);
+
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert(occupancies[i] == returns[i]);
+            }
+
+
+        }
+
+
+
     }
 }
