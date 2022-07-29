7b13cf4c39347ab6085afc5ab98e6b8e40bcb486
Michael Nowak <MJN56@pitt.edu>
2022-07-29 16:24:30 -0400

    Fixed unit tests.

--- Gog/gogTests/UnitTest1.cs
+++ Gog/gogTests/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace gogTests
 
             for(int i = 0; i < returns.Length; i++)
             {
-                Assert(occupancies[i] == returns[i]);
+                Assert.AreEqual(occupancies[i],returns[i]);
             }
 
             for (int i = 0; i < returns.Length; i++)
@@ -31,18 +31,53 @@ namespace gogTests
                 occupancies[i] = 1;
             }
 
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert.AreNotEqual(occupancies[i], returns[i]);
+            }
+
             RedlinePLC.SendOccupancies(occupancies);
             returns = RedlinePLC.ReceiveOccupancies(77);
 
             for (int i = 0; i < returns.Length; i++)
             {
-                Assert(occupancies[i] == returns[i]);
+                Assert.AreEqual(occupancies[i],returns[i]);
             }
 
 
         }
 
+        [TestMethod]
+        public void TrackReceiveOccupanciesGreen()
+        {
+            Track_Controller_1._02.Controller GreenLinePLC = new Track_Controller_1._02.Controller(4, true, "127.0.0.1");
 
+            int[] occupancies = new int[151];
+            int[] returns = new int[151];
+
+            GreenLinePLC.SendOccupancies(occupancies);
+            returns = GreenLinePLC.ReceiveOccupancies(151);
+
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert.AreEqual(occupancies[i],returns[i]);
+            }
+
+            for (int i = 0; i < returns.Length; i++)
+            {
+                occupancies[i] = 1;
+            }
+
+            GreenLinePLC.SendOccupancies(occupancies);
+            returns = GreenLinePLC.ReceiveOccupancies(151);
+
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert.AreEqual(occupancies[i],returns[i]);
+            }
+
+
+        }
 
     }
 }
