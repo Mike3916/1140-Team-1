69dad335a77ac235c42637fe3b4d6519b29f7a8c
jereifej <JHE7@pitt.edu>
2022-07-31 12:24:00 -0400

    Joe Tests -- Not Working

--- Gog/gogTests/UnitTest1.cs
+++ Gog/gogTests/UnitTest1.cs
@@ -65,43 +65,43 @@ namespace gogTests
 
         }
 
-        //[TestMethod]
-        //public void TrackReceiveOccupanciesGreen()
-        //{
-        //    Track_Controller_1._02.Controller GreenLinePLC = new Track_Controller_1._02.Controller(4, true, "127.0.0.1");
+        [TestMethod]
+        public void TrackReceiveOccupanciesGreen()
+        {
+            Track_Controller_1._02.Controller GreenLinePLC = new Track_Controller_1._02.Controller(4, true, "127.0.0.1");
 
-        //    int[] occupancies = new int[151];
-        //    int[] returns = new int[151];
+            int[] occupancies = new int[151];
+            int[] returns = new int[151];
 
-        //    GreenLinePLC.SendOccupancies(occupancies);
-        //    returns = GreenLinePLC.ReceiveOccupancies(151);
+            GreenLinePLC.SendOccupancies(occupancies);
+            returns = GreenLinePLC.ReceiveOccupancies(151);
 
-        //    for (int i = 0; i < returns.Length; i++)
-        //    {
-        //        Assert.AreEqual(occupancies[i],returns[i]);
-        //    }
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert.AreEqual(occupancies[i], returns[i]);
+            }
 
-        //    for (int i = 0; i < returns.Length; i++)
-        //    {
-        //        occupancies[i] = 1;
-        //    }
+            for (int i = 0; i < returns.Length; i++)
+            {
+                occupancies[i] = 1;
+            }
             for (int i = 0; i < returns.Length; i++)
             {
                 occupancies[i] = 1;
                 Assert.AreEqual(occupancies[i],returns[i]);
             }
 
-        //    GreenLinePLC.SendOccupancies(occupancies);
-        //    returns = GreenLinePLC.ReceiveOccupancies(151);
+            GreenLinePLC.SendOccupancies(occupancies);
+            returns = GreenLinePLC.ReceiveOccupancies(151);
 
-        //    for (int i = 0; i < returns.Length; i++)
-        //    {
-        //        Assert.AreEqual(occupancies[i],returns[i]);
-        //    }
+            for (int i = 0; i < returns.Length; i++)
+            {
+                Assert.AreEqual(occupancies[i], returns[i]);
+            }
 
 
-        //}
         }
+
         [TestMethod]
         public void TrackModel_AddTrain()
         {
@@ -117,5 +117,5 @@ namespace gogTests
             Assert.AreEqual(track.mtrainList[0].lineIdx, 0);
             Assert.AreEqual(track.mtrainList[0].commAuthority, 0);
         }
-    }
+    }   
 }
