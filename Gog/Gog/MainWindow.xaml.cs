755a8a49742eaa329d153f09db89e0f45451de31
jereifej <JHE7@pitt.edu>
2022-07-31 19:15:51 -0400

    UpdatePassengers

--- Gog/Gog/MainWindow.xaml.cs
+++ Gog/Gog/MainWindow.xaml.cs
@@ -89,7 +89,7 @@ namespace GogNS
 
         DispatcherTimer mGlobalTimer;
         int mIterationMultiplier = 10, numTrains = 0, iter = 0;
-        bool newBlock = false;
+        bool newBlock;
         bool gotTrack = false;
 
         public MainWindow()
@@ -161,18 +161,6 @@ namespace GogNS
             if (trainCtrl != null && trainCtrl.IsActive)
             {
                 Application.Current.MainWindow = trainCtrl;
-            }
-            else if (trains != null && trains.IsActive)
-            {
-                Application.Current.MainWindow = trains;
-            }
-            else if (track != null && track.IsActive)
-            {
-                Application.Current.MainWindow = track;
-            }
-            else if (ctc != null && ctc.IsActive)
-            {
-                Application.Current.MainWindow = ctc;
             }
         }
 
@@ -250,14 +238,18 @@ namespace GogNS
                     { 
                         TrackModel.Block bl = track.UpdateTrain(j); //get next block
                         if (bl != null)                             //if that block exists
+                        {
                             trains.UpdateBlock(bl, j);              //update the train pos
+                            if (bl.mStation)
+                            {
+                                track.SetPopulation(trains.UpdatePassenger(bl.mPop));
+                            }
+                        }
                         else
                         {                                         //if that block doesn't exist ...
                             trains.RemoveTrain(j);             //delete the train
                             track.RemoveTrain(j);              //remove the train from the track
                         }
-
-                        newBlock = false;
                     }
                 }
             }
